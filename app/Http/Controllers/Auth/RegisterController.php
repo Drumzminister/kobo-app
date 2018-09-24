8e880a45a54c669a162bc71f392f49fb0b688492
ekpono <ekponoambrose@gmail.com>
2018-09-24 13:51:05 +0100

    Email verification implemented

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -73,25 +73,25 @@ class RegisterController extends Controller
 
     public function verifyUser($token)
     {
-    $verifyUser = VerifyUser::where('token', $token)->first();
-    if(isset($verifyUser) ){
-        $user = $verifyUser->user;
-        if(!$user->verified) {
-        $verifyUser->user->verified = 1;
-        $verifyUser->user->save();
-        $status = "Your e-mail is verified. You can now login.";
-        } else {
-        $status = "Your e-mail is already verified. You can now login.";
+        $verifyUser = VerifyUser::where('token', $token)->first();
+        if(isset($verifyUser) ){
+            $user = $verifyUser->user;
+            if(!$user->verified) {
+                $verifyUser->user->verified = 1;
+                $verifyUser->user->save();
+                $status = "Your e-mail is verified. You can now login.";
+            } else {
+                $status = "Your e-mail is already verified. You can now login.";
+            }
+            } else {
+                return redirect('/login')->with('warning', "Sorry your email cannot be identified.");
+            }
+                return redirect('/login')->with('status', $status);
         }
-    } else {
-        return redirect('/login')->with('warning', "Sorry your email cannot be identified.");
-    }
-    return redirect('/login')->with('status', $status);
-    }
 
-    protected function registered(Request $request, $user)
-    {
-    $this->guard()->logout();
-    return redirect('/login')->with('status', 'We sent you an activation code. Check your email and click on the link to verify.');
-    }
+        protected function registered(Request $request, $user)
+        {
+            $this->guard()->logout();
+            return redirect('/login')->with('status', 'We sent you an activation code. Check your email and click on the link to verify.');
+        }
 }
