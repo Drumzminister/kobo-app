de8e93c42ff7d621d7ff7d6fc5a349c71d672670
James John James <jamesjay4199@gmail.com>
2018-10-22 10:43:07 +0100

    notigfication

---
+++ resources/views/notifications/sales.blade.php
@@ -0,0 +1,7 @@
+{{-- Send the notification variable containing a particular notification in this case it should be MadeSales notification --}}
+@php
+	$sale = Koboaccountant\Models\Sales::find($notification->data['sale_id']);
+@endphp
+<div>
+	{{$sale->company->name}} sold {{$sale->quantity}} quantity of  {{$sale->inventory->name}} for N{{$sale->amount}}
+</div>
