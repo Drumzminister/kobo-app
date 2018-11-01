69f1c7963f6426c0c913337a5cf1d9e5da17dc5a
ekpono <ekponoambrose@gmail.com>
2018-11-01 23:10:40 +0100

    added some base repository methods

--- app/Repositories/BaseRepository.php
+++ app/Repositories/BaseRepository.php
@@ -7,6 +7,18 @@ use Illuminate\Support\Facades\Auth;
 
 class BaseRepository {
 
+	/**
+	 * The model instance.
+	 *
+	 * @var \Illuminate\Database\Eloquent\Model
+	 */
+	protected $model;
+
+	public function __construct($model)
+	{
+		$this->model = $model;
+	}
+
 
     public function generateUuid()
     {
@@ -18,10 +30,10 @@ class BaseRepository {
         return str_replace('--', '-', strtolower(preg_replace('/[^a-zA-Z0-9]/', '-', trim($text))));  
     }
 
-    public function authUserId()
-    {
-        return Auth::id();
-    }
+	protected function getAuthUserId()
+	{
+		return Auth::user()->id;
+	}
 
     public function awsUpload($attachment) 
     {
@@ -34,6 +46,60 @@ class BaseRepository {
         // save to storage/app/photos as the new $filename
         $path = $file->storeAs('attachment', $filename);
 
-        return;
+        return $path;
     }
+
+    public function page($limit = 10, $offset = 0, array $relations = [], $orderBy = 'updated_at', $sorting = 'desc')
+	{
+		return $this->model->with($relations)->take($limit)->skip($offset)->orderBy($orderBy, $sorting)->get();
+    }
+    
+    public function findBy($attribute, $value, $relations = null)
+	{
+		$query = $this->model->where($attribute, $value);
+		if ($relations && is_array($relations)) {
+			foreach ($relations as $relation) {
+				$query->with($relation);
+			}
+		}
+		return $query->firstOrFail();
+    }
+    
+    	/**
+	 * Get all records by an associative array of attributes.
+	 * Two operators values are handled: AND | OR.
+	 *
+	 * @param array  $attributes
+	 * @param string $operator
+	 * @param array  $relations
+	 *
+	 * @return \Illuminate\Support\Collection
+	 */
+	public function getByAttributes(array $attributes, $operator = 'AND', $relations = null)
+	{
+		// In the following it doesn't matter with element to start with, in all cases all attributes will be appended to the
+		// builder.
+		// Get the last value of the associative array
+		$lastValue = end($attributes);
+		// Get the last key of the associative array
+		$lastKey = key($attributes);
+		// Builder
+		$query = $this->model->where($lastKey, $lastValue);
+		// Pop the last key value pair of the associative array now that it has been added to Builder already
+		array_pop($attributes);
+		$method = 'where';
+		if (strtoupper($operator) === 'OR') {
+			$method = 'orWhere';
+		}
+		foreach ($attributes as $key => $value) {
+			$query->$method($key, $value);
+		}
+		if ($relations && is_array($relations)) {
+			foreach ($relations as $relation) {
+				$query->with($relation);
+			}
+		}
+		return $query->get();
+    }
+    
 }
