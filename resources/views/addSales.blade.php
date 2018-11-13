7d858fb58df14be909b54125cd23ca951c56a410
syfon01 <syfonisaac@gmail.com>
2018-11-13 11:16:11 +0100

    add sales page

--- resources/views/addSales.blade.php
+++ resources/views/addSales.blade.php
@@ -170,12 +170,12 @@
 
                     <div class="row p-3 mt-2 ">
                         <div class="col-md-6">
-                            <div class="bg-grey pt-5 pb-5 px-3">
+                            <div class="bg-grey pt-5 pb-5 px-3" id="topp">
                                 <div class="row">
                                     <div class="col-md-5">
                                         <h5 class="h5 uppercase">Payment Mode</h5>
                                         <div class="dropdown show mt-3">
-                                                <a class="btn btn-lg btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                                <a class="btn btn-lg btn-payment dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                                   Bank (GTB)
                                                 </a>
                                               
@@ -209,7 +209,7 @@
                                 <div class="row">
                                         <div class="col-md-5">
                                             <div class="dropdown show mt-3">
-                                                    <a class="btn btn-lg btn-secondary dropdown-toggle px-4" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                                    <a class="btn btn-lg btn-payment dropdown-toggle " href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                                       Cash
                                                     </a>
                                                   
@@ -242,82 +242,51 @@
 
                     {{-- total sum section --}}
                         <div class="col-md-6">
-                                <div class="bg-grey pt-5 pb-5 px-3">
+                                <div class="bg-grey pt-5 pb-3 px-3" id="topp">
                                         <div class="row">
-                                            <div class="col-md-5">
+                                            <div class="col-md-6">
                                                 <h5 class="h5 uppercase">Payment Mode</h5>
-                                                <div class="dropdown show mt-3">
-                                                        <a class="btn btn-lg btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                                          Bank (GTB)
-                                                        </a>
-                                                      
-                                                        <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                                                          <a class="dropdown-item" href="#">GTB 1</a>
-                                                          <a class="dropdown-item" href="#">GTB 2</a>
-                                                          <a class="dropdown-item" href="#">Skye Bank</a>
-                                                        </div>
-                                                      </div>
+                                                <div class="input-group mb-3 input-group-lg">
+                                                    <div class="input-group-prepend">
+                                                        <span class="input-group-text customer-input" id="basic-addon3">&#8358;</span>
+                                                    </div>
+                                                    <input type="text" class="form-control " id="basic-url" aria-describedby="basic-addon3" placeholder="100,000">                        
+                                                </div>
                                             </div>
 
-                                            <div class="col-md-5">
+                                            <div class="col-md-6">
                                                 <h5 class="h5 uppercase">Amount</h5>
-                                                <div class="dropdown show mt-3">
-                                                        <a class="btn btn-lg btn-secondary dropdown-toggle " href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                                          500,000
-                                                        </a>
-                                                      
-                                                        <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                                                          <a class="dropdown-item" href="#">200,000</a>
-                                                          <a class="dropdown-item" href="#">100,000</a>
-                                                          <a class="dropdown-item" href="#">50,000</a>
-                                                        </div>
-                                                      </div>
-                                        
+                                                <div class="input-group mb-3 input-group-lg">
+                                                    <div class="input-group-prepend">
+                                                        <span class="input-group-text customer-input" id="basic-addon3">&#8358;</span>
+                                                    </div>
+                                                    <input type="text" class="form-control " id="basic-url" aria-describedby="basic-addon3" placeholder="100,000">                        
+                                                </div>
+                            
                                             </div>
-                                            <div class="col-md-2"></div>
                                         </div>
                                                                                 
-                                        <div class="row">
-                                                <div class="col-md-5">
-                                                    <div class="dropdown show mt-3">
-                                                            <a class="btn btn-lg btn-secondary dropdown-toggle px-4" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                                              Cash
-                                                            </a>
-                                                          
-                                                            <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                                                              <a class="dropdown-item" href="#">ATM</a>
-                                                              <a class="dropdown-item" href="#">GTB 2</a>
-                                                              <a class="dropdown-item" href="#">Skye Bank</a>
-                                                            </div>
-                                                    </div>
+                                        <div class="row pt-4">
+                                            <div class="col">
+                                            <h5 class="h5 uppercase">Total Amount</h5>
+                                            </div>
+                                            <div class="col input-group mb-3 input-group-lg">
+                                                <div class="input-group-prepend cus">
+                                                    <span class="input-group-text customer-input" id="basic-addon3">&#8358;</span>
                                                 </div>
-
-                                                <div class="col-md-5">
-                                                    <div class="dropdown show mt-3">
-                                                            <a class="btn btn-lg btn-secondary dropdown-toggle " href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                                              750,000
-                                                            </a>
-                                                          
-                                                            <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                                                              <a class="dropdown-item" href="#">500,000</a>
-                                                              <a class="dropdown-item" href="#">200,000</a>
-                                                              <a class="dropdown-item" href="#">50,000</a>
-                                                            </div>
-                                                          </div>
-                                            
-                                                </div>
-                                                <div class="col-md-2">  </div>            
+                                                <input type="text" class="form-control " id="basic-url" aria-describedby="basic-addon3" placeholder="1,275,000">                        
+                                            </div>            
                                         </div>
                                     </div>
                         </div>
                         {{-- end of total sum section --}}                    
                     </div>  
                     {{-- end of entire payment section --}}
-                    
+
                     {{-- payment buttons --}}
                     <div class="row p-5">
                         <div class="col">                           
-                            <a href="" class="btn btn-lg btn-secondary">Send Invoice</a>
+                            <a href="" class="btn btn-lg btn-login">Send Invoice</a>
                         </div>
                         <div class="col">
                             <span class="float-right">
