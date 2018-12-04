6ebd606352375e5a7adb45454ea64ee25db06ce0
syfon01 <syfonisaac@gmail.com>
2018-12-04 20:26:01 +0100

    @

--- routes/web.php
+++ routes/web.php
@@ -79,6 +79,26 @@ Route::get('/debtor', function () {
     return view('debtors');
 });
 
+Route::get('/debt', function () {
+    return view('debt');
+});
+
+Route::get('/view-debtor', function () {
+    return view('view-debtor');
+});
+
+// creditors
+Route::get('/creditor', function () {
+    return view('creditors');
+});
+
+Route::get('/credit', function () {
+    return view('credit');
+});
+
+Route::get('/view-creditor', function () {
+    return view('view-creditor');
+});
 
 
 
