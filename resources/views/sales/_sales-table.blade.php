0951ec114647608403586e0d49a9812bb2940447
ekpono <ekponoambrose@gmail.com>
2018-12-08 15:17:25 +0100

    updates

--- resources/views/sales/_sales-table.blade.php
+++ resources/views/sales/_sales-table.blade.php
@@ -119,59 +119,7 @@
                     {{-- end of current payment --}}
 
                     {{-- total sum section --}}
-                        <div class="col-md-6">
-                                <div class="bg-grey py-4 px-3" id="topp">
-                                        <div class="row">
-                                            <div class="col-md-6">
-                                                <h5 class="h5 uppercase">Total Discount</h5>
-                                                <div class="input-group mb-3 input-group-lg">
-                                                    <div class="input-group-prepend">
-                                                        <span class="input-group-text customer-input" id="basic-addon3">&#8358;</span>
-                                                    </div>
-                                                    <input type="text" class="form-control " id="basic-url" aria-describedby="basic-addon3" placeholder="100,000">                        
-                                                </div>
-                                            </div>
-
-                                            <div class="col-md-6">
-                                                <h5 class="h5 uppercase">Total Delivery Amount</h5>
-                                                <div class="input-group mb-3 input-group-lg">
-                                                    <div class="input-group-prepend">
-                                                        <span class="input-group-text customer-input" id="basic-addon3">&#8358;</span>
-                                                    </div>
-                                                    <input type="text" class="form-control " id="basic-url" aria-describedby="basic-addon3" placeholder="100,000">                        
-                                                </div>
-                            
-                                            </div>
-                                        </div>
-                                                                                
-                                        <div class="row pt-2">
-                                            <div class="col">
-                                            <h5 class="h5 uppercase">Total Amount</h5>
-                                            </div>
-                                            <div class="col input-group input-group-lg">
-                                                <div class="input-group-prepend cus">
-                                                    <span class="input-group-text customer-input" id="basic-addon3">&#8358;</span>
-                                                </div>
-                                                <input type="text" class="form-control " id="basic-url" aria-describedby="basic-addon3" placeholder="1,275,000">                        
-                                            </div>            
-                                        </div>
-                                    </div>
-                        </div>
-                        {{-- end of total sum section --}}                    
-                    </div>  
-                    {{-- end of entire payment section --}}
-
-                    {{-- payment buttons --}}
-                    <div class="row p-3">
-                        <div class="col">                           
-                            <a href="" class="btn btn-lg btn-login" data-toggle="modal" data-target="#exampleModalCenter">Send Invoice</a>
-                        </div>
-                        <div class="col">
-                            <span class="float-right">
-                                <a href="" class="btn btn-lg btn-started">Save</a>
-                            </span>
-                        </div>
-                    </div>
+                    @include('sales._payment')
             </div> 
            
         </div>
