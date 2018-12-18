1477770a2dcf3265ce2ffd0966108a378806e3ec
syfon01 <syfonisaac@gmail.com>
2018-12-18 17:13:24 +0100

    customer update

--- resources/views/add-customers.blade.php
+++ resources/views/add-customers.blade.php
@@ -1,84 +1,85 @@
+@extends('layouts.app')
 
-@extends("layouts.app-acct")
+@section('content')
 
-@section("content")
-{{-- heading section --}}
 <section id="top">
-        <div class="container p-2">
-            <div class="row p-3">
-                <h2><a href="/customers" class="text-dark"> Customers</a></h2>
-                <span class="accountant ml-auto btn btn-accountant">
-                <a href="" class="btn-accountant">
-                    <img src="https://res.cloudinary.com/samuelweke/image/upload/v1527079189/profile.png"> Accountant
-                </a>              
-                </span>
-            </div>
+    <div class="container py-3">
+        <div class="row ">
+            <h2> <a href="/customers" class="text-dark">Customer</a></h2>            
         </div>
-    </section>
-{{-- end of heading section --}}
+    </div>
+</section>
 
-<section id="sale-table">
-        <div class="container mt-4">             
-            <div class="bg-white p-4">
-                
-                <div class="table-responsive table-responsive-sm">
-                    <table class="table table-striped table-hover" id="dataTable">
-                        <thead class="p-3">
-                          <tr class="tab">
-                            <th scope="col">Customer Name</th>
-                            <th scope="col">Address</th>                                    
-                            <th scope="col">Phone No</th>
-                            <th scope="col">Email</th>
-                            <th scope="col">Website</th>
-                          </tr>
-                        </thead>
-                        <tbody>
-                          <tr>
-                                <td >Amaka Ekpono</td>                                       
-                                <td>8A lagos state  </td>                                   
-                                <td> 08082023909</td>
-                                <td> syfon@gmail.com</td>
-                                <td>koboaccountant.com</td>
-                          </tr>
+<section>
+    <div class="container my-4">
+        <div class="bg-white p-5">
+            <img src="{{asset('img/person.png')}}" alt="client logo" srcset="" class="rounded-circle img-fluid">
+            <h5 class="h5">Add Photo</h5>
 
-                            <tr>
-                                    <td >Amaka Ekpono</td>                                       
-                                    <td>8A lagos state  </td>                                   
-                                    <td> 08082023909</td>
-                                    <td> syfon@gmail.com</td>
-                                    <td>koboaccountant.com</td>
-                            <tr>
-                                    <td >Amaka Ekpono</td>                                       
-                                    <td>8A lagos state  </td>                                   
-                                    <td> 08082023909</td>
-                                    <td> syfon@gmail.com</td>
-                                    <td>koboaccountant.com</td>
-                            <tr>
-                                    <td >Amaka Ekpono</td>                                       
-                                    <td>8A lagos state  </td>                                   
-                                    <td> 08082023909</td>
-                                    <td> syfon@gmail.com</td>
-                                    <td>koboaccountant.com</td>
-                            <tr>
-                                    <td >Amaka Ekpono</td>                                       
-                                    <td>8A lagos state  </td>                                   
-                                    <td> 08082023909</td>
-                                    <td> syfon@gmail.com</td>
-                                    <td>koboaccountant.com</td>
-                                </tr>
-                            
-                        </tbody>
-                </table>
-                <span class="float-right" >Add Row <i class="fa fa-plus-square" style="font-size:24px;color:#00C259;"></i>
-                </span> 
-             </div>
-             <div class="text-center pb-3">
-                    <a href="" class="btn btn-started">Save &amp; Continue</a> 
+            <form>
+                <div class="form-group row py-2">
+                    <div class="col-md-4">
+                        <label for="name" class="col-md-3 col-form-label">Account</label>
+                        <p class="text-muted">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel, natus!</p>
+                    </div>
+                    <div class="col-md-8">
+                        <label for="first name">First Name</label>
+                        <input type="text" class="form-control bg-grey" id="" >
+                        <label for="last name">Last name</label>
+                        <input type="text" class="form-control bg-grey" id="" >
+                        <label for="phone">Phone number</label>
+                        <input type="number" class="form-control bg-grey" id="" >
+                    </div>
                 </div>
-            </div> 
-           
-        </div>
-    </section>
+                
+                <hr>
 
-  
+                <div class="form-group row py-2">
+                    <div class="col-md-4">
+                        <label for="decription" class="col-form-label">Address</label>
+                        <p class="text-muted">Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, totam?</p>
+                    </div>
+                    <div class="col-md-8">
+                        <label for="Role">Address</label>
+                        <select name="" id="" class="form-control bg-grey">
+                                <option value="" selected></option>
+                                <option value="role"></option>
+                                <option value="role"></option>
+                                <option value="role"></option>
+                        </select>
+                        <span class="float-right mt-1" onclick="" style="cursor: pointer;">Add Row <i class="fa fa-plus-square" style="font-size:32px; color:#00C259;" value="Add Row"></i>
+                        </span>
+                    </div>
+                </div>
+                
+
+                <div class="form-group row py-2">
+                        <div class="col-md-4">
+                            <label for="decription" class="col-form-label">Website</label>
+                            <p class="text-muted">Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, totam?</p>
+                        </div>
+                        <div class="col-md-8">
+                            <label for="Role">Website Name</label>
+                            <select name="" id="" class="form-control bg-grey">
+                                    <option value="" selected></option>
+                                    <option value="role"></option>
+                                    <option value="role"></option>
+                                    <option value="role"></option>
+                            </select>
+                            
+                        </div>
+                    </div>
+                    <div class="form-row mt-3">
+                        <div class="col-md-4"></div>
+                        <div class="col-md-8">
+                            <button type="submit" class="btn btn-addsale">Save Information</button>
+                        </div>
+                    </div>
+            </form>
+        </div>
+    </div>    
+</section> 
+
+
+
 @endsection
