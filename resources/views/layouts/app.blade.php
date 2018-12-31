1923ace613deb85911b0d4dbb0135b7a399335c4
ekpono <ekponoambrose@gmail.com>
2018-12-31 19:14:27 +0100

    fixes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -68,8 +68,6 @@
 <!-- Date Picker CDN -->
 <link rel="stylesheet" href="http://code.jquery.com/ui/1.9.1/themes/base/jquery-ui.css" />
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.min.js"></script>
-<link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/css/select2.min.css" rel="stylesheet" />
-<script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/js/select2.min.js"></script>
 
 <script>
 var introguide = introJs();
--- resources/views/sales/_vat.blade.php
+++ resources/views/sales/_vat.blade.php
@@ -42,6 +42,3 @@
         </div>
 </div>
 </section>
-<!-- This Calls ajax and displays all customers -->
-<link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/css/select2.min.css" rel="stylesheet" />
-<script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/js/select2.min.js"></script>
