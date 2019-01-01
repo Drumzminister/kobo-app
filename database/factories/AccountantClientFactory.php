e57112614d1b8cc15a200abe51678d8abad9eea7
Olatunbosun Egberinde <bosunski@gmail.com>
2019-01-01 18:23:15 +0100

    Modify some migration and seeders

--- database/factories/AccountantClientFactory.php
+++ database/factories/AccountantClientFactory.php
@@ -1,9 +1,10 @@
 <?php
 
+use App\Data\AccountantClient;
 use Faker\Generator as Faker;
 
 
-$factory->define(\App\Data\AccountantClient::class, function (Faker $faker) {
+$factory->define(AccountantClient::class, function (Faker $faker) {
 	return [
 		'id' => $faker->uuid,
 	];
