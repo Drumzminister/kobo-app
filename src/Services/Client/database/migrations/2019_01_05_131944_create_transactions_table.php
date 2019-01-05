67b15325fbcf778328ca44f97a8ffa07028cb691
Olatunbosun Egberinde <bosunski@gmail.com>
2019-01-05 22:28:18 +0100

    Rename a migration

--- src/Services/Client/database/migrations/2019_01_05_131944_create_transactions_table.php
+++ src/Services/Client/database/migrations/2019_01_05_131944_create_transactions_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTransactionTable extends Migration
+class CreateTransactionsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateTransactionTable extends Migration
      */
     public function up()
     {
-        Schema::create('transaction', function (Blueprint $table) {
+        Schema::create('transactions', function (Blueprint $table) {
             $table->string('id', 36);
             $table->string('kobo_id', 36)->unique();
             $table->string('sale_id', 36)->nullable();
@@ -36,6 +36,6 @@ class CreateTransactionTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('transaction');
+        Schema::dropIfExists('transactions');
     }
 }
