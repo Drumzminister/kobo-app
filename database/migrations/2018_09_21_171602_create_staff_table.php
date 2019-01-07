8f58b48d9dc229afa445067beac469e9b521c78b
Olatunbosun Egberinde <bosunski@gmail.com>
2019-01-07 15:14:30 +0100

    Modify a some seeders and migrations

--- database/migrations/2018_09_21_171602_create_staff_table.php
+++ database/migrations/2018_09_21_171602_create_staff_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateStaffsTable extends Migration
+class CreateStaffTable extends Migration
 {
 
     public function up()
