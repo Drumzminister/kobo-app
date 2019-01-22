f89375508b9ad0ea31a56707bbcd18d0610cd008
Olatunbosun Egberinde <bosunski@gmail.com>
2019-01-22 21:30:59 +0100

    minor Update

--- src/Data/Repositories/SalesTransaction.php
+++ src/Data/Repositories/SalesTransaction.php
@@ -4,7 +4,11 @@ use App\Data\Repositories\TransactionRepository;
 
 class SalesTransactionRepository extends TransactionRepository
 {
-	public function saveTransaction(array $data)
+	public function saveTransaction(array $data, $sale)
 	{
+		$data['sale_id'] = $sale->id;
+		$data['kobo_id'] = explode('-', $this->generateUuid())[0];
+
+		return $this->fillAndSave($data);
 	}
 }
