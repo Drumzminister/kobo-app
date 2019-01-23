d99e8b7fa8aab35a9f2e069d11f7be9c1afb4a9d
James John James <jamesjay4199@gmail.com>
2019-01-23 16:01:10 +0100

    loan validation and fixes

--- resources/js/mixins/loan.js
+++ resources/js/mixins/loan.js
@@ -1,3 +1,5 @@
+import {toast} from "../helpers/alert";
+
 export const loanApp = {
     data: {
         loans: [],
@@ -18,6 +20,7 @@ export const loanApp = {
         loanDescription: "",
         loanPeriod: "month",
         noSourceFound: false,
+        allLoanIntervals: [],
         loanPaymentAmount: "",
         showSourcesForm: false,
         sourceSearching: false,
@@ -25,10 +28,12 @@ export const loanApp = {
         currentLoanPayments: [],
         isRequestingLoan: false,
         showMoreIntervals: false,
-        accountReceivingLoan: null,
         loadingLoanDetails: false,
+        accountReceivingLoan: null,
+        loanPaymentIntervalList: [],
         showIntervalSelector: false,
         loanPaymentInterval: "Monthly",
+        showSelectPaymentMode: false,
         loanPaymentValidationMessage: "",
         loanPaymentValidationError: false
     },
@@ -50,7 +55,20 @@ export const loanApp = {
             } else{
                 this.loanPaymentValidationError = false;
             }
+        },
+        loanInterest () {
+            if (Number(this.loanInterest) >= 100) {
+                toast('Interest can not be greater than 100%', 'error');
+                this.loanInterest = this.loanInterest.slice(0, this.loanInterest.length - 1) ;
+            }
+        },
+        loanTerm () {
+            this.calculateIntervalsToBeShown();
+        },
+        loanPeriod () {
+            this.calculateIntervalsToBeShown();
         }
+
     },
     mounted () {
         this.loans = window.loans;
@@ -59,11 +77,71 @@ export const loanApp = {
         this.loanAmtPaid = window.loanAmtPaid;
         this.loanAmtOwing = window.loanAmtOwing;
         this.loanAmtRunning = window.loanAmtRunning;
+        this.allLoanIntervals = [
+            {
+                name: "Weekly" ,
+                value: "1 week"
+            },
+            {
+                name: "Bi-weekly",
+                value: "2 week"
+            },
+            {
+                name: "Monthly",
+                value: "1 month"
+            },
+            {
+                name: "Bi-monthly",
+                value: "2 month"
+            },
+            {
+                name: "Quaterly",
+                value: "3 month"
+            },
+            {
+                name: "Anually",
+                value: "1 year"
+            },
+            {
+                name: "4 Months",
+                value: "4 month"
+            },
+            {
+                name: "5 Months",
+                value: "5 month"
+            },
+            {
+                name: "6 Months",
+                value: "6 month"
+            },
+            {
+                name: "7 Months",
+                value: "7 month"
+            },
+            {
+                name: "8 Months",
+                value: "8 month"
+            },
+            {
+                name: "9 Months",
+                value: "9 month"
+            },
+            {
+                name: "10 Months",
+                value: "10 month"
+            },
+            {
+                name: "11 Months",
+                value: "11 month"
+            },
+        ];
+        this.loanPaymentIntervalList = [...this.allLoanIntervals];
+        this.loanPaymentInterval = this.loanPaymentIntervalList[0];
         this.runDebouncedSearch = _.debounce(this.searchForSource, 500);
     },
     computed: {
         selectedAccounts () {
-            return this.$store.state.selectedAccounts;
+            return this.$store.getters.selectedAccounts;
         }
     },
     methods: {
@@ -129,7 +207,7 @@ export const loanApp = {
             formData.append('amount', this.loanAmount);
             formData.append('period', this.loanPeriod);
             formData.append('term', this.loanTerm);
-            formData.append('payment_interval', this.paymentPerYear);
+            formData.append('payment_interval', this.loanPaymentInterval.value);
             formData.append('start_date', this.loanDate);
             formData.append('receivingAccount', JSON.stringify(this.accountReceivingLoan) );
             this.isRequestingLoan = true;
@@ -179,12 +257,13 @@ export const loanApp = {
             this.isRequestingLoan = true;
             axios.post(`/client/loan/${this.currentLoan.id}/pay`, formData).then(response => {
                 this.isRequestingLoan = false;
-                this.closeModal('#paymentModal');
                 this.closeModal('#loanDetailsModal');
-                swal('Success', `Payment made successfully`, 'success');
+                this.currentLoan.amount_paid = Number(this.currentLoan.amount_paid) + Number(sum);
+                toast(`Payment made successfully`, 'success');
+                this.showSelectPaymentMode = !this.showSelectPaymentMode
             }).catch(err => {
                 this.isRequestingLoan = false;
-                swal('Oops', `${err.response.data.message}`, 'error');
+                toast(`${err.response.data.message}`, 'error');
             });
         },
         toggleShowMoreIntervals (evt) {
@@ -202,8 +281,8 @@ export const loanApp = {
         toggleShowIntervalSelector () {
             this.showIntervalSelector = !this.showIntervalSelector;
         },
-        selectLoanPaymentInterval (event) {
-            this.loanPaymentInterval = event.target.innerText;
+        selectLoanPaymentInterval (interval) {
+            this.loanPaymentInterval = interval;
             this.toggleShowIntervalSelector();
         },
         closeLoanModal () {
@@ -216,6 +295,21 @@ export const loanApp = {
             this.loanInterest = "";
             this.paymentPerYear = 1;
             this.closeModal('#addLoanModal');
+        },
+        calculateIntervalsToBeShown () {
+            if (this.loanTerm.trim() === "") {
+                this.loanPaymentIntervalList = [...this.allLoanIntervals];
+            }
+            if (this.loanTerm) {
+                let duration = moment([]).add(this.loanTerm, this.loanPeriod);
+                this.loanPaymentIntervalList = this.allLoanIntervals.filter(interval => {
+                    let term, period;
+                    [term, period] = interval.value.split(" ");
+                    let nextInterval = moment([]).add(term, period);
+                    return duration.isSameOrAfter(nextInterval);
+                });
+                this.loanPaymentInterval = this.loanPaymentIntervalList[0];
+            }
         }
     }
 };
