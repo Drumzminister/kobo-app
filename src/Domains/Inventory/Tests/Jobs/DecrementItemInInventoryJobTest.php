7bb79f20859db8c30db288ad075c3d32066d4f63
Olatunbosun Egberinde <bosunski@gmail.com>
2019-01-24 02:59:28 +0100

    minor Update

---
+++ src/Domains/Inventory/Tests/Jobs/DecrementItemInInventoryJobTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Domains\Inventory\Tests\Jobs;
+
+use App\Domains\Inventory\Jobs\DecrementItemInInventoryJob;
+use Tests\TestCase;
+
+class DecrementItemInInventoryJobTest extends TestCase
+{
+    public function test_decrement_item_in_inventory_job()
+    {
+        $this->markTestIncomplete();
+    }
+}
---
+++ src/Domains/Inventory/Tests/Jobs/IncrementItemInInventoryJobTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Domains\Inventory\Tests\Jobs;
+
+use App\Domains\Inventory\Jobs\IncrementItemInInventoryJob;
+use Tests\TestCase;
+
+class IncrementItemInInventoryJobTest extends TestCase
+{
+    public function test_increment_item_in_inventory_job()
+    {
+        $this->markTestIncomplete();
+    }
+}
