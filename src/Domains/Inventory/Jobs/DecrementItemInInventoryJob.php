ab1ece55f7871d082aae5b78fbce629b61795293
syfon01 <syfonisaac@gmail.com>
2019-01-28 14:52:28 +0100

    front end fix on dashboard

---
+++ src/Domains/Inventory/Jobs/DecrementItemInInventoryJob.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Domains\Inventory\Jobs;
+
+use App\Data\SaleItem;
+use Koboaccountant\Traits\HelpsResponse;
+use Lucid\Foundation\Job;
+
+class DecrementItemInInventoryJob extends Job
+{
+	use HelpsResponse;
+
+	/**
+	 * @var
+	 */
+	private $sale;
+
+	/**
+	 * Create a new job instance.
+	 *
+	 * @param $sale
+	 */
+    public function __construct($sale)
+    {
+    	$this->sale = $sale;
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle()
+    {
+        $items = $this->sale->saleItems;
+
+        if ($this->sale->type !== 'published') {
+        	return $this->createJobResponse('error', 'Drafted Sale cannot update Inventory', null);
+        }
+
+        $items->each(function (SaleItem $item) {
+        	$inventory = $item->inventory;
+        	$inventory->quantity = $inventory->quantity - $item->quantity;
+        	$inventory->save();
+        });
+
+        return $this->createJobResponse('success', 'Inventory updated successfully', null);
+    }
+}
---
+++ src/Services/Client/Tests/Features/HandleCsvUploadFeatureTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Services\Client\Tests\Features;
+
+use Tests\TestCase;
+use App\Services\Client\Features\HandleCsvUploadFeature;
+
+class HandleCsvUploadFeatureTest extends TestCase
+{
+    public function test_handlecsvuploadfeature()
+    {
+        $this->markTestIncomplete();
+    }
+}
