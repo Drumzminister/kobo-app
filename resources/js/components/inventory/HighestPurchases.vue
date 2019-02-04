5f13157bde8be8e4e9086db927e0f083422725dc
ekpono <ekponoambrose@gmail.com>
2019-02-04 04:07:27 +0100

    refactor on inventory

---
+++ resources/js/components/inventory/HighestPurchases.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="col-md-4">
+        <div class="bg-white p-2 " id="topp">
+            <div class="row my-1">
+                <div class="col mt-1">
+                    <h5 class="h5">Top 10 Purchases</h5>
+                </div>
+                <div class="col">
+                    <select id="" @change="highestPurchase" class="form-control btn-filter pull-right">
+                        <option selected>Quantity <i class="fa fa-filter"></i></option>
+                        <option  class="text-small">By Amount</option>
+                    </select>
+                </div>
+            </div>
+            <div class="all-scroll">
+                <table class="table table-striped table-hover" id="table">
+                    <thead class="sale-head">
+                    <tr>
+                        <th scope="col">Products</th>
+                        <th scope="col">Quantity</th>
+                        <th scope="col">Amount</th>
+                    </tr>
+                    </thead>
+                    <tbody class="tbody">
+                    <tr v-for="purchase in top_purchase">
+                        <td>@{{  purchase.name }}</td>
+                        <td>@{{ purchase.quantity }}</td>
+                        <td>@{{ purchase.sales_price }}</td>
+                    </tr>
+                    </tbody>
+
+                </table>
+            </div>
+            <h3 v-if="top_purchase.length === 0"class="text-center">
+                Top purchases will appear here
+            </h3>
+            <div class="text-center p-1">
+                <a href="" class="view-more">View More Analytics</a>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
