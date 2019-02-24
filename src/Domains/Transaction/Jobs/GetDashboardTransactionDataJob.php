d393720ef164084bacc808fb1a5d48a5f1721297
Olatunbosun Egberinde <bosunski@gmail.com>
2019-02-25 00:57:22 +0100

    Update Job and dashboard page

--- src/Domains/Transaction/Jobs/GetDashboardTransactionDataJob.php
+++ src/Domains/Transaction/Jobs/GetDashboardTransactionDataJob.php
@@ -1,8 +1,9 @@
 <?php
 
-namespace App\Domains\Debtor\Jobs;
+namespace App\Domains\Transaction\Jobs;
 
 use App\Data\Repositories\DebtorRepository;
+use App\Data\Repositories\Transaction;
 use App\Data\Repositories\TransactionRepository;
 use Koboaccountant\Http\Resources\DebtorCollection;
 use Lucid\Foundation\Job;
@@ -15,7 +16,7 @@ class GetDashboardTransactionDataJob extends Job
     private $user;
 
     /**
-     * @var \Illuminate\Foundation\Application|DebtorRepository
+     * @var \Illuminate\Foundation\Application|Transaction
      */
     private $transaction;
 
@@ -41,37 +42,32 @@ class GetDashboardTransactionDataJob extends Job
             abort(404);
         }
 
-        $debtors = $this->transaction->getPublishedSalesOrderedByDate($company->id);
+        $transactions = $this->transaction->getAndOrderedByDate($company->id);
 
-        $firstDebtor = $debtors->first();
+        $firstDebtor = $transactions->first();
 
-        $dayDebtors   = $this->createDebtorCollectionsFromDebtors($debtors->whereBetween('created_at', [now()->subDay(), now()]));
-        $weekDebtors  = $this->createDebtorCollectionsFromDebtors($debtors->whereBetween('created_at', [now()->subWeek(), now()]));
-        $monthDebtors = $this->createDebtorCollectionsFromDebtors($debtors->whereBetween('created_at', [now()->subMonth(), now()]));
-        $yearDebtors  = $this->createDebtorCollectionsFromDebtors($debtors->whereBetween('created_at', [now()->subYear(), now()]));
+        $dayTransactions   = $this->createDebtorCollectionsFromTransactions($transactions->whereBetween('created_at', [now()->subDay(), now()]));
+        $weekTransactions  = $this->createDebtorCollectionsFromTransactions($transactions->whereBetween('created_at', [now()->subWeek(), now()]));
+        $monthTransactions = $this->createDebtorCollectionsFromTransactions($transactions->whereBetween('created_at', [now()->subMonth(), now()]));
+        $yearTransactions  = $this->createDebtorCollectionsFromTransactions($transactions->whereBetween('created_at', [now()->subYear(), now()]));
 
 
-        $debtors = $this->createDebtorCollectionsFromDebtors($debtors);
+        $transactions = $this->createDebtorCollectionsFromTransactions($transactions);
 
 
         return [
-            'totalInvoice'      => $debtors->map(function ($debtor) { return $debtor->sale->total_amount; })->sum(),
-            'totalPaid'         => $debtors->map(function ($debtor) { return $debtor->sale->transactions->pluck('amount')->sum(); })->sum(),
-            'debtTotal'         => $debtors->pluck('amount')->sum(),
-            'debtors'           => $debtors,
-            'monthDebtors'      => $monthDebtors,
-            'dayDebtors'        => $dayDebtors,
-            'weekDebtors'       => $weekDebtors,
-            'yearDebtors'       => $yearDebtors,
-            'startDate'         => $firstDebtor ? $firstDebtor->created_at : now()->toDateString(),
+            'transactions'           => $transactions,
+            'monthTransactions'      => $monthTransactions,
+            'dayTransactions'        => $dayTransactions,
+            'weekTransactions'       => $weekTransactions,
+            'yearTransactions'       => $yearTransactions,
+            'startDate'              => $firstDebtor ? $firstDebtor->created_at : now()->toDateString(),
         ];
     }
 
-    protected function createDebtorCollectionsFromDebtors($debtors)
+    protected function createDebtorCollectionsFromTransactions($transactions)
     {
-        return $debtors->map(function ($debtor)  {
-            return new DebtorCollection($debtor);
-        });
+        return $transactions;
 
     }
 }
