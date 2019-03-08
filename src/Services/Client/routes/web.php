463fc14fc6bfc46c8c966a78bd1b21879945ec44
Olatunbosun Egberinde <bosunski@gmail.com>
2019-03-08 17:45:30 +0100

    Update SaleListView.js

--- src/Services/Client/routes/web.php
+++ src/Services/Client/routes/web.php
@@ -20,7 +20,7 @@ Route::group([ 'prefix' => 'client'], function () {
 
     Route::get('/', function () {
         $sale = new \Koboaccountant\Http\Resources\SaleCollection($saleX = \Koboaccountant\Models\Sale::find('21f873a6-36e1-11e9-8c32-b33f538c67ae'));
-//        dd($saleX->transactions->pluck('amount')->sum());
+//      dd($saleX->transactions->pluck('amount')->sum());
         $data['sale'] = (object) $sale->toArray(request());
 	    $html = view('client::pdf.invoice', $data)->render();
 	    $pdf = \Illuminate\Support\Facades\App::make('snappy.pdf.wrapper');
